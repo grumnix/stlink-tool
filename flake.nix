482d8b3913c1407511ecc11bd83c99b56df49cdc
Ingo Ruhnke <grumbel@gmail.com>
2023-05-23 20:38:53 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     stlink-tool_src.url = "git+https://github.com/UweBonnes/stlink-tool.git?submodules=1";
